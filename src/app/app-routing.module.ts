2f9be3cc5ec7995e405844a3c038cc5b3c832377
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-12-09 17:14:58 -0300

    Setting up app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { OrderComponent } from './orders/order/order.component';
+import { OrdersComponent } from './orders/orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'orders', component: OrdersComponent },
+{
+  path: 'order', children: [
+    { path: '', component: OrderComponent },
+    { path: 'edit', component: OrderComponent }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { OrderItemsComponent } from './orders/order-items/order-items.component';
+import { OrderComponent } from './orders/order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    OrderItemsComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
