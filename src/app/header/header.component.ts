3ed296ce2266ba582c247e941fdd7a609dd08787
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-12-11 16:38:00 -0300

    set up header when scrolling

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  isActive = false;
+  handleScroll() {
+    const scrollY = window.scrollY
+    if (scrollY > 10) {
+      document.getElementById("header")?.classList.add("scrollClass")
+      this.isActive = true
+    } else {
+      document.getElementById("header")?.classList.remove("scrollClass")
+      this.isActive = false
+    }
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+    window.addEventListener("scroll", this.handleScroll);
   }
 
 }
