909bc0c22d03599b9f91ab4f906d05759b14fd8b
Regis Rufino <regis.rfnrodrigues@gmail.com>
2020-12-12 17:52:35 -0300

    creating order form

--- src/app/orders/order/order.component.ts
+++ src/app/orders/order/order.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { OrderService } from 'src/app/shared/order.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-order',
@@ -11,6 +12,15 @@ export class OrderComponent implements OnInit {
 
   constructor(public service: OrderService) { }
 
+  formLabels = {
+    orderId: "Order ID:",
+    orderNo: "Order Nº:",
+    customerId: "Customer Id:",
+    paymentMethod: "Payment",
+    totalValue: "Total Value"
+
+  }
+
   ngOnInit(): void {
     this.resetForm()
   }
@@ -20,12 +30,16 @@ export class OrderComponent implements OnInit {
       form.resetForm();
     this.service.formData = {
       orderId: undefined,
-      orderNo: undefined,
+      orderNo: uuidv4(),
       customerId: undefined,
       paymentMethod: undefined,
       totalValue: undefined
 
     }
+  }
+
+  trackByFn(index: any, item: any) {
+    return index;
   }
 
 }
