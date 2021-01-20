edfba89b050e74733cf8e6ee1aaf868a62a54719
Regis Rufino <regis.rfnrodrigues@gmail.com>
2021-01-20 12:40:09 -0300

    refactoring style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { OrderComponent } from './orders/order/order.component';
 import { OrdersComponent } from './orders/orders.component';
 
-const routes: Routes = [{ path: 'orders', component: OrdersComponent },
-{
-  path: 'order', children: [
-    { path: '', component: OrderComponent },
-    { path: 'edit', component: OrderComponent }
-  ]
-}];
+const routes: Routes = [
+  { path: '', component: OrdersComponent },
+  {
+    path: 'order', children: [
+      { path: '', component: OrderComponent },
+      { path: 'edit', component: OrderComponent }
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
